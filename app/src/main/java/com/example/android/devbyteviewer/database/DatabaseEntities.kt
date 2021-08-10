922a7561288449e3f163b12a20e93db2d6b2f3c9
Joseph <osekuemen@gmail.com>
2021-08-10 21:52:52 +0100

    Step 1 - Add a DatabaseVideo Entity

--- app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
@@ -17,3 +17,41 @@
 
 package com.example.android.devbyteviewer.database
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.android.devbyteviewer.domain.Video
+import com.example.android.devbyteviewer.network.NetworkVideoContainer
+
+@Entity
+data class DatabaseVideo constructor(
+    @PrimaryKey
+    val url: String,
+    val updated: String,
+    val title: String,
+    val description: String,
+    val thumbnail: String)
+
+// an extension function which converts from database objects to domain objects:
+fun List<DatabaseVideo>.asDomainModel(): List<Video> {
+    return map {
+        Video (
+            url = it.url,
+            title = it.title,
+            description = it.description,
+            updated = it.updated,
+            thumbnail = it.thumbnail)
+    }
+}
+
+//Finally, in network/DataTransferObjects.kt, create an extension function that converts
+// from data transfer objects to database objects:
+fun NetworkVideoContainer.asDatabaseModel(): Array<DatabaseVideo> {
+    return videos.map {
+        DatabaseVideo (
+            title = it.title,
+            description = it.description,
+            url = it.url,
+            updated = it.updated,
+            thumbnail = it.thumbnail)
+    }.toTypedArray()
+}
--- app/src/main/java/com/example/android/devbyteviewer/ui/DevByteFragment.kt
+++ app/src/main/java/com/example/android/devbyteviewer/ui/DevByteFragment.kt
@@ -197,7 +197,6 @@ class DevByteAdapter(val callback: VideoClick) : RecyclerView.Adapter<DevByteVie
             it.videoCallback = callback
         }
     }
-
 }
 
 /**
