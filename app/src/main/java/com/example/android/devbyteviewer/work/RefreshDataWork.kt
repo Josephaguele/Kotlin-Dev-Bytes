7b70f8d67c38d01dea2659eae0b4af84bf8f6e53
Joseph <osekuemen@gmail.com>
2021-08-13 07:04:32 +0100

    Step 8 - Create a worker

--- app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
+++ app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -14,5 +14,42 @@
  * limitations under the License.
  *
  */
+/*
+*You're going to upgrade DevByteViewer to pre-fetch data when the app is in the background.
+* You should use the WorkManager library to accomplish this.
+After you’ve completed this exercise and the next one, your users will get the latest data every
+*  time they open the app. WorkManager will make sure to schedule the work so it has the lowest
+*  impact on battery life possible.
+* */
 
 package com.example.android.devbyteviewer.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
+import retrofit2.HttpException
+
+/*In work/RefreshDataWorker.kt, add the RefreshDataWorker class. WorkManager workers always
+extend a Worker class. We're going to use a CoroutineWorker, because we want to use coroutines
+ to handle our asynchronous code and threading. Have RefreshDataWorker extend from the
+ CoroutineWorker class. You also need to pass a Context and WorkerParameters to the class and its
+ parent class.*/
+class RefreshDataWorker(appContext: Context, params: WorkerParameters):
+    CoroutineWorker(appContext, params) {
+    //This is what "work" your RefreshDataWorker does, in our case,
+    // syncing with the network. Add variables for the database and the repository.
+    // The doWork() function now returns Result instead of Payload because they have combined Payload into Result.
+    // Read more here - https://developer.android.com/jetpack/androidx/releases/work#1.0.0-alpha12
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (e: HttpException) {
+            Result.retry()
+        }
+    }
+}
